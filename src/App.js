462e226749e1f1756738f6a78ea08835023992db
heeyupchung <heeyupchung@gmail.com>
2024-10-31 11:09:21 -0700

    add unsplash, hit api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom";
+import React, { Fragment, useEffect, useState } from "react";
+import { createApi } from "unsplash-js";
 
-function App() {
+const api = createApi({
+  // Don't forget to set your access token here!
+  // See https://unsplash.com/developers
+  accessKey: "r-Cir8lI-ewNJ9R2E-WArbdxtZgKTy2sMwoxQF-s3Iw"
+});
+
+const PhotoComp = ({ photo }) => {
+  const { user, urls } = photo;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <img className="img" src={urls.regular} />
+      <a
+        className="credit"
+        target="_blank"
+        href={`https://unsplash.com/@${user.username}`}
+      >
+        {user.name}
+      </a>
+    </Fragment>
   );
-}
+};
+
+const Body = () => {
+  const [data, setPhotosResponse] = useState(null);
+
+  useEffect(() => {
+    api.search
+      .getPhotos({ query: "ocean", orientation: "landscape" })
+      .then(result => {
+        setPhotosResponse(result);
+      })
+      .catch(() => {
+        console.log("something went wrong!");
+      });
+  }, []);
+
+  if (data === null) {
+    return <div>Loading...</div>;
+  } else if (data.errors) {
+    return (
+      <div>
+        <div>{data.errors[0]}</div>
+        <div>PS: Make sure to set your access token!</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="feed">
+        <ul className="columnUl">
+          {data.response.results.map(photo => (
+            <li key={photo.id} className="li">
+              <PhotoComp photo={photo} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
+
+const App = () => {
+  return (
+    <main className="root">
+      <Body />
+    </main>
+  );
+};
 
 export default App;
