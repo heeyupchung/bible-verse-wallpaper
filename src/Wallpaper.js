52c466b49cc70b8a8aae85b01158e73085b64a12
heeyupchung <heeyupchung@gmail.com>
2024-11-06 19:08:29 -0800

    add dummy verses to app, add hide actions button, add functionality, style verses

--- src/Wallpaper.js
+++ src/Wallpaper.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { createApi } from "unsplash-js";
 import { Button, FormControl, TextField } from "@mui/material";
+import { dummyVerses } from "./getVerses";
 
 export default function Wallpaper() {
   const [data, setPhotosResponse] = useState(null);
   const [randPhoto, setRandPhoto] = useState(null);
   const [randomize, setRandomize] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
+  const [verse, setVerse] = useState("");
+  const [hideActions, setHideActions] = useState(false);
 
   const api = createApi({
     accessKey: "r-Cir8lI-ewNJ9R2E-WArbdxtZgKTy2sMwoxQF-s3Iw"
@@ -15,6 +18,9 @@ export default function Wallpaper() {
 
   useEffect(() => {
     if(randomize) {
+      let random = Math.floor(Math.random() * dummyVerses.length);
+      setVerse(dummyVerses[random]);
+
       api.search
       .getPhotos({ query: !searchQuery ? "ocean" : searchQuery, orientation: "landscape" })
       .then(result => {
@@ -43,23 +49,39 @@ export default function Wallpaper() {
   } else {
     const backgroundClasses = {
       "backgroundImage": `url(${randPhoto.urls.regular})`, 
-      // "backgroundRepeat": "no-repeat", 
-      // "backgroundSize": "cover", 
+      "backgroundRepeat": "no-repeat", 
+      "backgroundSize": "cover", 
       "backgroundPosition": "center", 
       "backgroundAttachment": "fixed", 
       "height": "100vh"
     }
 
     return (
-      <form onSubmit={e => { e.preventDefault(); setRandomize(true);}} className="wallpaper-background"
-        style={backgroundClasses}>
-        {/* <img className="img" src={randPhoto.urls.regular} /> */}
-        <div className="actions">
-          <TextField size="small" label="Search" variant="filled" value={searchQuery} onChange={e => setSearchQuery(e.target.value)}/>
-          <Button variant="contained" type="submit" >
-            New Wallpaper
-          </Button>
+      <form onSubmit={e => { e.preventDefault(); setRandomize(true);}} className="wallpaper-background" style={backgroundClasses}>
+        <div className="header">
+          <span className=""></span>
+          <span className="header-actions">
+            <span className="download"></span>
+            <span className="change-orientation"></span>
+            <span className="tint-background"></span>
+            <span className="font-white-or-black"></span>
+            {/* <span className="hide-all-actions">
+              <Button variant="contained" onClick={() => setHideActions(!hideActions)}>{hideActions ? "Show" : "Hide"} Actions</Button>
+            </span> */}
+          </span>
         </div>
+        <div className="bible-verse">
+          <div className="verse-content">{verse.content}</div>
+          <div className="verse-reference">{verse.reference}</div>
+        </div>
+        {hideActions ? null : 
+          <div className="actions">
+            <TextField size="small" label="Search" variant="filled" value={searchQuery} onChange={e => setSearchQuery(e.target.value)}/>
+            <Button variant="contained" type="submit" >
+              Search
+            </Button>
+          </div>
+        }
       </form>
     );
   }
---
+++ src/getVerses.js
@@ -0,0 +1,38 @@
+export const dummyVerses = [    
+    {
+      content: "Jesus Christ is the same yesterday, today, and forever.",
+      reference: "Hebrews 13:8"
+    },
+    {
+      content: "You keep him in perfect peace whose mind is stayed on you because he trusts in you.",
+      reference: "Isaiah 26:3"
+    },
+    {
+      content: "But this I call to mind, and therefore I have hope: The steadfast love of the Lord never ceases; his mercies never come to an end; they are new every morning; great is your faithfulness.",
+      reference: "Lamentations 3:21-23"
+    },
+    {
+      content: "I have been crucified with Christ. It is no longer I who live, but Christ who lives in me. The life I now live in the flesh I live by faith in the Son of God who loved me and gave himself for me.",
+      reference: "Galatians 2:20"
+    },
+    {
+      content: "For by grace you have been saved, through faith, and this is not your own doing. It is the gift of God, not a result of works, so that no one may boast.",
+      reference: "Ephesians 2:8-9"
+    },
+    {
+      content: "All scripture is breathed out by God and profitable for teaching, for reproof, for correction, and for training in righteousness, that the man of God may be complete, equipped for every good work.",
+      reference: "2 Timothy 3:16-17"
+    },
+    {
+      content: "But God shows his love for us in that while we were still sinners, Christ died for us.",
+      reference: "Romans 5:8"
+    },
+    {
+      content: "For all have sinned, and fall short of the glory of God,",
+      reference: "Romans 3:23"
+    },
+    {
+      content: "For even the Son of Man came not to be served, but to serve and to give his life as a ransom for many.",
+      reference: "Mark 10:45"
+    }
+];
